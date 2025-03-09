0d4224241c2f88429778c3c9e6c00cb1f41ded12
heezi <themiyakasun.tb@gmail.com>
2025-03-09 21:55:30 +0530

    About Overview

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,14 @@ import React from "react";
 
 import Overview from "@/components/Overview";
 import FeaturesOverview from "@/components/FeaturesOverview";
+import AboutOverview from "@/components/AboutOverview";
 
 const Page = () => {
   return (
     <>
       <Overview />
       <FeaturesOverview />
+      <AboutOverview />
     </>
   );
 };
---
+++ components/AboutOverview.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+
+const AboutOverview = () => {
+  return (
+    <div className="overview w-full">
+      <div className="root-container mt-20 mb-20">
+        <div className="mx-auto max-w-7xl flex flex-col md:flex-row gap-10 items-center">
+          <div className="flex flex-1 flex-col gap-5 relative mt-10 md:mt-0">
+            <Image
+              src="/images/about.jpg"
+              alt="about"
+              width={500}
+              height={600}
+            />
+          </div>
+          <div className="flex flex-1 flex-col gap-5">
+            <div className="flex items-center gap-2 float-left">
+              <div className="border-t-2 border-t-[var(--color-dark)] w-5"></div>
+              <h4 className="text-sm md:text-lg text-[var(--color-dark)] font-medium">
+                About Us
+              </h4>
+            </div>
+
+            <h1 className="text-black text-3xl md:text-5xl">
+              Choose the Best IT Solution Company
+            </h1>
+            <p className="text-black/80 text-lg">
+              Finding the right IT solution partner is essential for business
+              growth. We provide expertise in software development, cloud
+              computing, cybersecurity, and AI solutions to help businesses
+              thrive. Our focus on innovation, scalability, and security ensures
+              that we deliver cutting-edge technology tailored to your needs.
+              Partner with us for reliable, future-ready IT solutions that drive
+              success.
+            </p>
+            <div>
+              <Button variant="default" className=" font-bold h-12">
+                Learn More
+              </Button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default AboutOverview;
