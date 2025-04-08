46fb1d33f899e68e5cf100f25578676c286040b5
tescobit <hello@tescobit.com>
2025-04-08 20:10:01 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    output: 'export',
-  assetPrefix: '/tescobit.github.io/official-website/',
-  images: {
-    unoptimized: true,
-  },
 };
 
 export default nextConfig;
